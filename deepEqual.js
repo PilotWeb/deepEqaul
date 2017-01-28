9e1192b20691ab78608a0a2dbfbe78ff460bd62f
eugene <piloteg@list.ru>
2017-01-28 13:00:24 +0600

    Latest

---
+++ deepEqual.js
@@ -0,0 +1,23 @@
+function deepEqual(obj1, obj2){
+ if(typeof obj1 == 'object' && typeof obj2 == 'object' && obj1 !==null && obj2!==null){
+  for(var value in obj2){
+    console.log(value);
+    if(!(value in obj1)) return false;
+    
+  }
+  for(var value in obj1){
+     console.log(value);
+     if(value in obj2){
+     if(deepEqual(obj1[value], obj2[value])){}
+       else{return false;};
+     }else{
+       return false;
+     }     
+    }
+   return true;
+ }else if(obj1 === obj2){
+  return true; 
+ } else{
+  return false;
+ }
+}
